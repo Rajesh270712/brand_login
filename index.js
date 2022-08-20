46a0331eaf5be221a1cf14a8e0e640d07b76919d
Rajesh Bagul <rajeshbagul2001@gmail.com>
2022-08-20 17:30:32 +0530

    update

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ require('dotenv').config()
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.send({
-//     message: "connected",
-//   });
-// });
+app.get("/", (req, res) => {
+  res.send({
+    message: "connected",
+  });
+});
 
 app.use(signUpRoute);
 app.use(loginRoute);
